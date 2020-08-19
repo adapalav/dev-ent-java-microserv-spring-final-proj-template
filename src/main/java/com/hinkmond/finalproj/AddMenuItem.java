24ce127c147539bd635ba9e6ce6fb61ac2890245
adapalav <adapalav@yahoo.com>
2020-08-18 19:00:28 -0700

    First shot at the restaurant menu db, spring boot, cloud app

---
+++ src/main/java/com/hinkmond/finalproj/AddMenuItem.java
@@ -0,0 +1,48 @@
+package com.hinkmond.finalproj;
+
+
+public class AddUserData {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String usrName;
+    private String phoneNo;
+    private String address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setUsrName(String usrName) {
+        this.usrName = usrName;
+    }
+    public String getUsrName() {
+        return usrName;
+    }
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
